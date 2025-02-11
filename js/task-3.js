75938c955bd23408fa6a8f3f75ce46eb47be5849
tanya-yanko <yanko200662@gmail.com>
2025-02-11 11:02:48 +0200

    done hw

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+    let result = [];
+    for (let num of numbers) {
+        if (num > value) {
+            result.push(num);
+        }
+    }
+    return result;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20)); 
